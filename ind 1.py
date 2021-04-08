1c94aabe696b469ffc554dbf6857da0eef8b007d
turutle <sennheiser.thomson@gmail.com>
2021-04-08 23:49:53 +0300

    Added individual tasks

---
+++ ind 1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 22:23:58 2021
+
+@author: turtl
+"""
+
+import sys
+
+
+if __name__ == '__main__':
+    x = int(input("По какой цене вы купили 8 рулонов(обоев)--> "))
+    y = int(input("По какой цене вы купили 2 банки краски--> "))
+    n = x + y
+    
+    if n > 200 and n < 500:
+        print(n*0.97)
+    elif n > 500 and n < 800:
+        print(n*0.95)
+    elif n > 800 and n < 1000:
+        print(n*0.93)
+    elif n > 1000:
+        print(n*0.91)
+    else:
+        print("Ошибка!", file=sys.stderr)
+        exit(1)
---
+++ ind 2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 23:05:32 2021
+
+@author: turtl
+"""
+
+import sys
+
+
+if __name__ == '__main__':
+    x = int(input("Введите первое число--> "))
+    y = int(input("Введите второе число--> "))
+    z = int(input("Введите третье число--> "))
+    
+    if x % 2 == 0:
+        print("Первое число чётное")
+    elif y % 2 == 0:
+        print("Второе число чётное")
+    elif z % 2 == 0:
+        print("Третье число чётное")
+    elif x % 2 == 1 or y % 2 == 1 or z % 2 == 1:
+        print("Cреди чисел нет чётных")
+    else:
+        print("Ошибка!", file=sys.stderr)
+        exit(1)
