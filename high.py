b66c66f6091c4c8f3549b1b59cc12a753b0e7aa2
turutle <sennheiser.thomson@gmail.com>
2021-04-11 23:36:00 +0300

    Final edit

---
+++ high.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 11 16:00:06 2021
+
+@author: turtl
+"""
+
+import math
+
+EPS = 1e-10
+
+if __name__ == '__main__':
+    x = float(input("Введите x--> "))
+    
+    if x < 0 or x > 2:
+        print("Illegal value of x")
+    else:
+        a = 1 - x
+        S = a
+        k = 1
+    
+        while math.fabs(a) > EPS:
+            a *= ((1 - x) * k**2) / (k**2 + k*2 + 1)
+            S += a
+            k += 1
+            
+        print(f"f({x}) = {S}")
--- ind 1.py
+++ ind 1.py
@@ -23,4 +23,3 @@ if __name__ == '__main__':
         print(n*0.91)
     else:
         print("Ошибка!", file=sys.stderr)
-        exit(1)
