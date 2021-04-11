669d636857abfab7bb3b101833fbad31ff537be5
turutle <sennheiser.thomson@gmail.com>
2021-04-11 15:44:49 +0300

    Done

--- ind 3.py
+++ ind 3.py
@@ -10,15 +10,15 @@ k = 0
 t = 0
 s = 0
 if __name__ == '__main__':
-    while s <= 100:
+    while b < 1:
         s += 10
         b += 1
-        
-        s += 10
-        k += 2
-        
-        s += 10
-        t += 20
+    while k < 9:
+        s += 5
+        k += 1
+    while t < 89:
+        s += 1
+        t += 2
     print(b)
     print(k)
     print(t)
